b938dcb87f10692416fb58e4be62c550c688b099
MatheusBrizza <matheusbj95@gmail.com>
2022-05-13 21:32:04 -0300

    Enunciado no README.md e Exercício no index.js

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # node-ktztvf
 
+Considere o objeto `medicos` declarado abaixo.
+
+```jsx
+const medicos = [
+  {
+    nome: 'Leticia',
+    sobrenome: 'Costa',
+    genero: 'feminino',
+  },
+  {
+    nome: 'Adimaldo',
+    sobrenome: 'Pinto',
+    genero: 'masculino',
+  },
+  {
+    nome: 'Noah',
+    sobrenome: 'Lutus',
+    genero: 'nao-binario',
+  },
+];a
+```
+
+Faça uma função que irá retornar um array de strings. Essa função deve formatar o nome do médico conforme o `genero`. Veja os templates abaixo. Use o map.
+
+`nao-binario` Dr(e) `nome` + `sobrenome` \*
+
+`masculino` Dr. `nome` + `sobrenome`
+
+`feminino` Dr(a) `nome` + `sobrenome`
+
+\*espero que esteja certo 🙏
+
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-ktztvf)
--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 // run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const medicos = [
+  {
+    nome: 'Leticia',
+    sobrenome: 'Costa',
+    genero: 'feminino',
+  },
+  {
+    nome: 'Adimaldo',
+    sobrenome: 'Pinto',
+    genero: 'masculino',
+  },
+  {
+    nome: 'Noah',
+    sobrenome: 'Lutus',
+    genero: 'nao-binario',
+  },
+];
+
+const nomesMedicos = (medico) => {
+  if (medico.genero === 'masculino') {
+    const masculino = {
+      nome: 'Dr. ' + medico.nome + ' ' + medico.sobrenome,
+    };
+  } else if (medico.genero === 'feminino') {
+    const feminino = {
+      nome: 'Dr(a). ' + medico.nome + ' ' + medico.sobrenome,
+    };
+  } else {
+    const naoBinario = {
+      nome: 'Dr(e). ' + medico.nome + ' ' + medico.sobrenome,
+    };
+  }
+};
+
+const medicoss = 
